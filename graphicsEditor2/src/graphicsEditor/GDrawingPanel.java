51e9985385fc24cff4360b605ad258a8d4f4f9ff
chois <chois@LG_Gram_2022>
2025-03-31 11:35:50 +0900

    20250331

--- graphicsEditor2/src/graphicsEditor/GDrawingPanel.java
+++ graphicsEditor2/src/graphicsEditor/GDrawingPanel.java
@@ -2,6 +2,9 @@ package graphicsEditor;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
@@ -9,6 +12,9 @@ public class GDrawingPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public GDrawingPanel() {
+		MouseEventHandler mouseEventHandler = new MouseEventHandler();
+		this.addMouseListener(mouseEventHandler);
+		this.addMouseMotionListener(mouseEventHandler);
 	}
 
 
@@ -23,5 +29,42 @@ public class GDrawingPanel extends JPanel {
 	public void draw(Graphics graphics) {
 		graphics.setColor(Color.RED);
 		graphics.drawRect(10, 10, 50, 50);
+	}
+	
+	private class MouseEventHandler implements MouseMotionListener, MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			System.out.println("mouseClicked");
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println("mousePressed");
+		}
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			System.out.println("mouseMoved");
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println("mouseDragged");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			System.out.println("mouseReleased");
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			System.out.println("mouseEntered");
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			System.out.println("mouseExited");
+		}
+		
 	}
 }
